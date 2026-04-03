0d5477a7f708351d39ec76405ded49310e35a0bc
Tyler-Reagan <tylerreagan98@gmail.com>
2026-04-03 15:05:24 -0700

    image and output widget shifts

--- boards/shields/dongle_screen/src/widgets/output_status.c
+++ boards/shields/dongle_screen/src/widgets/output_status.c
@@ -117,7 +117,7 @@ int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_ob
     lv_obj_set_style_text_font(widget->transport_label, &lv_font_montserrat_14, 0);
 
     widget->ble_label = lv_label_create(widget->obj);
-    lv_obj_align(widget->ble_label, LV_ALIGN_TOP_RIGHT, -10, 56);
+    lv_obj_align(widget->ble_label, LV_ALIGN_TOP_RIGHT, -10, 40);
     lv_obj_set_style_text_font(widget->ble_label, &lv_font_montserrat_14, 0);
 
     sys_slist_append(&widgets, &widget->node);
